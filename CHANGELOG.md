04a5af80b7b9850b1f55aad459f0d7115ca83fc1
BH1XUW <bh1xuw@gmail.com>
2020-03-23 04:36:45 +0800

    chore: add items to CHANGELOG

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 ## Unreleased
+### Added
+- New examples
+
+### Changed
+- Use edition 2018
+- Link against RocksDB 6.7.3
 
 ## 0.1.5
 ### Changed
--- examples/hello.rs
+++ examples/hello.rs
@@ -2,7 +2,6 @@ use rocks::prelude::*;
 
 fn main() {
     let opt = Options::default().map_db_options(|db_opt| db_opt.create_if_missing(true));
-
     let db = DB::open(opt, "./data").unwrap();
 
     assert!(db.put(WriteOptions::default_instance(), b"hello", b"world").is_ok());
@@ -10,5 +9,5 @@ fn main() {
         Ok(ref value) => println!("hello: {:?}", value),
         Err(e) => eprintln!("error: {}", e),
     }
-    let _ = db.delete(&WriteOptions::default(), b"hello").unwrap();
+    let _ = db.delete(WriteOptions::default_instance(), b"hello").unwrap();
 }
