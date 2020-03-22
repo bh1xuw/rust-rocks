687de836ab836643d9c06f2b107502d3098ba621
BH1XUW <bh1xuw@gmail.com>
2020-03-22 20:57:03 +0800

    save and test

--- rocks-sys/build.rs
+++ rocks-sys/build.rs
@@ -257,6 +257,7 @@ mod imp {
             cfg.cxxflag(format!("-I{}", src.join("snappy").display()));
             // snappy-stubs-public.h
             cfg.cxxflag(format!("-I{}", snappy_build_dir.join("build").display()));
+            println!("cargo:warning=dir={}", snappy_build_dir.join("build").display());
         }
 
         #[cfg(feature = "zlib")]
