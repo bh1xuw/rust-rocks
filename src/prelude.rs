73ccf22d44fdae9a34c391ff9b1eb7526f859112
BH1XUW <bh1xuw@gmail.com>
2020-05-10 16:51:37 +0800

    refactor(version): use struct Version

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,9 +1,5 @@
 //! The `RocksDB` prelude entry.
 
-use rocks_sys as ll;
-
-pub use self::ll::version;
-
 pub use crate::comparator::Comparator;
 pub use crate::db::*;
 pub use crate::env::{Env, Logger};
@@ -15,13 +11,7 @@ pub use crate::table::*;
 pub use crate::table_properties::{TableProperties, TablePropertiesCollection};
 pub use crate::transaction_log::LogFile;
 pub use crate::types::SequenceNumber;
+pub use crate::version::version;
 pub use crate::write_batch::WriteBatch;
 
 pub use super::Error;
-
-#[test]
-fn test_version() {
-    let v = version();
-    println!("version = {}", v);
-    assert!(v >= "5.3.1".into());
-}
